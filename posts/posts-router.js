220d629f14b44c9d9bd7b832e4bd68a08f59fdbd
mlazzell17@gmail.com <MicheSi>
2020-02-11 15:42:39 -0700

    about to update post, mvp completed

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -105,4 +105,27 @@ router.delete('/:id', (req, res) => {
     }
 })
 
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+    const {id} = req.params;
+
+    if(!changes.title || !changes.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        Posts.update(id, changes)
+        .then(post => {
+            if (post) {
+            res.status(200).json(post);
+            } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist."} )
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "The post information could not be modified." })
+    })
+    }
+    
+})
+
 module.exports = router;
