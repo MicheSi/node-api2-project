0720e9e04421c414d92b35a7dbab95cde59e79e7
mlazzell17@gmail.com <MicheSi>
2020-02-11 15:13:46 -0700

    able to post comments

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -47,21 +47,42 @@ router.get('/:id/comments', (req, res) => {
     })
 })
 
-// router.post('/', (req, res) => {
-//     const postInfo = req.body;
+router.post('/', (req, res) => {
+    const postInfo = req.body;
 
-//     if (!postInfo.title || !postInfo.contents) {
-//         res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
-//     } else {
-//         Posts.insert(postInfo)
-//         .then(post => {
-//             res.status(201).json(post);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json({ error: "There was an error while saving the post to the database" });
-//         });
-//     }
-// })
+    if (!postInfo.title || !postInfo.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        Posts.insert(postInfo)
+        .then(post => {
+            res.status(201).json(post);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "There was an error while saving the post to the database" });
+        });
+    }
+})
+
+router.post('/:id/comments', (req, res) => {
+    const {id} = req.params;
+    const comment = {...req.body, post_id: id}
+
+    if (!id) {
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    }
+     else if (!comment.text) {
+        res.status(400).json({ errorMessage: "Please provide text for the comment." })
+    } else {
+        Posts.insertComment(comment)
+        .then(post => {
+            res.status(200).json(post)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "There was an error while saving the comment to the database" })
+        })
+    }
+})
 
 module.exports = router;
