8c0434b444b441aa4efedcc3c4dcbd826e0be25b
mlazzell17@gmail.com <MicheSi>
2020-02-11 13:34:36 -0700

    added get to posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,5 +4,27 @@ const Posts = require('../data/db');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    Posts.find(req.query)
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+router.post('/', (req, res) => {
+    
+    Posts.add(req.body)
+    .then(post => {
+        res.status(201).json(post);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ errorMessage: 'Unable to add post' });
+    });
+})
 
 module.exports = router;
