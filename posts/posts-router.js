df5e3e13fb806fd041e09490e14523cf3d68163a
mlazzell17@gmail.com <MicheSi>
2020-02-11 14:01:09 -0700

    able to get by post id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -15,16 +15,37 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
-    
-    Posts.add(req.body)
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    Posts.findById(id)
     .then(post => {
-        res.status(201).json(post);
+        if (!post) {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } else {
+            res.status(200).json(post)
+        }
     })
     .catch(err => {
         console.log(err);
-        res.status(500).json({ errorMessage: 'Unable to add post' });
-    });
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    })
 })
+
+// router.post('/', (req, res) => {
+//     const postInfo = req.body;
+
+//     if (!postInfo.title || !postInfo.contents) {
+//         res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+//     } else {
+//         Posts.insert(postInfo)
+//         .then(post => {
+//             res.status(201).json(post);
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json({ error: "There was an error while saving the post to the database" });
+//         });
+//     }
+// })
 
 module.exports = router;
