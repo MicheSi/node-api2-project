93869fb0828be1d190ea2affd126b3a92a18047e
mlazzell17@gmail.com <MicheSi>
2020-02-11 17:11:24 -0700

    got posts rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import PostsList from './components/PostsList';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Stretch Project</h1>
+      <PostsList />
     </div>
   );
 }
---
+++ client/src/components/PostsCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PostsCard = props => {
+    return (
+        <div className='postsCard'>
+            <h2>{props.title}</h2>
+        <   p>{props.contents}</p>
+        </div>
+    )
+}
+
+export default PostsCard;
